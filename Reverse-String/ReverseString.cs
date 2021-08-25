2684d94d35c3cc0699e074fbd17c1a1d5f655048
olepunchy <74633934+olepunchy@users.noreply.github.com>
2021-08-25 15:18:59 -0500

    Added Reverse-String project

---
+++ Reverse-String/ReverseString.cs
@@ -0,0 +1,13 @@
+﻿// using System;
+
+public static class ReverseString {
+    public static string Reverse(string input) {
+        string reverse = string.Empty;
+
+        for (int index = input.Length - 1; index >= 0; index--) {
+            reverse += input[index];
+        }
+
+        return reverse;
+    }
+}
