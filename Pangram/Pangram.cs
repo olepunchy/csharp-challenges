f2fadb0a36cc3925de44af6882ecbc527b9855d4
olepunchy <74633934+olepunchy@users.noreply.github.com>
2021-08-26 15:22:03 -0500

    Added Pangram project

---
+++ Pangram/Pangram.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using System.Text.RegularExpressions;
+
+public static class Pangram {
+
+    private const string _alphabet = "abcdefghijklmnopqrstuvwxyz";
+    private const string _regex = "[^a-zA-Z]+";
+
+    public static bool IsPangram(string input) {
+
+        input = Regex.Replace(input.ToLower(), _regex, "");
+
+        if (string.IsNullOrEmpty(input) || input.Length < 26) {
+            return false;
+        }
+
+        int missingCount = 0;
+
+        for (int index = 0; index < _alphabet.Length; index++) {
+            bool result = input.Any(a => a == _alphabet[index]);
+            if (!result) {
+                missingCount++;
+            }
+        }
+
+        return missingCount == 0;
+    }
+}
